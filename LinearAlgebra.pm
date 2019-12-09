efc8bafd350038b563d16f903d4e76ce27f6a3d3
Grégory Vanuxem <gvanuxem.perl@gmail.com>
2019-12-09 01:08:43 +0000

    support Lapack >= 3.7.0

--- LinearAlgebra.pm
+++ LinearAlgebra.pm
@@ -6882,11 +6882,11 @@ sub PDL::Complex::mgsvd {
 
 =head1 AUTHOR
 
-Copyright (C) Grégory Vanuxem 2005-2007.
+Copyright (C) Grégory Vanuxem 2005-2018.
 
 This library is free software; you can redistribute it and/or modify
-it under the terms of the artistic license as specified in the Artistic
-file.
+it under the terms of the Perl Artistic License as in the file Artistic_2
+in this distribution.
 
 =cut
 
